cf4b21f017ce7aa2a2d9ad5fc0a3934d1c83cef4
manish <manishgupta09830@gmail.com>
2023-01-18 18:03:49 +0530

    bind method and storing email in an object .

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ let student={
   firstName:"manish",
   lastName:"gupta",
   age:18,
-  getEmail:getEmail(this.firstName,this.lastName)
 }
 let teacher={
   firstName:"promod",
   lastName:"tiwari",
   age:18,
-  getEmail:getEmail(this.firstName,this.lastName)
 }
 function getEmail(firstName,lastName){
-  return `${this.firstName}${this.lastName}@gmail.com`
+  return this.email= `${this.firstName}${this.lastName}@gmail.com`
 }
 function chooseSubject(sub1,sub2){
   console.log(sub1,sub2);
   
 }
-console.log(chooseSubject.apply(teacher,["maths"]));
-console.log(chooseSubject.call(student,["maths","arts"]));
+// console.log(chooseSubject.apply(teacher,["maths"]));
+// console.log(chooseSubject.call(student,["maths","arts"]));
+// console.log(getEmail.bind(teacher));
+
+ 
+let callanotherTime=getEmail.bind(teacher);
+console.log(callanotherTime());
+console.log(teacher);
