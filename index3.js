ba9f01d6a9637f583b14c9a004b3dc14d27184d3
manish <manishgupta09830@gmail.com>
2023-01-19 18:31:14 +0530

    advance clouser example

---
+++ index3.js
@@ -0,0 +1,16 @@
+
+var name = (arr)=>{
+    var count=0;
+  return ()=>{
+    console.log("hello"+arr[count]);
+    count++;
+  }
+
+
+}
+
+let fun = name(["Ram","Shyam"]);
+
+fun()// Print Hello Ram
+
+fun()//print Hello Shyam
